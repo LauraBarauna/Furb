7a532e9da700e182f10d7159aba65adb6974a612
Laura Barauna <lauravbarauna@gmail.com>
2025-03-01 15:38:08 -0300

    feat: complete Exercise 15

--- IP2025/Unidade2/Exe15/src/Exercicio15.java
+++ IP2025/Unidade2/Exe15/src/Exercicio15.java
@@ -4,13 +4,80 @@ public class Exercicio15 {
     public static void main(String[] args) {
         
         double valor;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Digite o valor: ");
         valor = scanner.nextDouble();
 
-        int vInt = valor * 100;
+        int valorSemVirgula = (int)(valor*100);
+        int reais = valorSemVirgula / 100;
+        int centavos = valorSemVirgula % 100;
+        double centavosD = valorSemVirgula % 100;
 
-        System.out.println(v);
+        // Reais
+
+        int qtdNotaCem = reais / 100;
+        reais = reais % 100;
+
+        int qtdNotaCinquena = reais / 50;
+        reais = reais % 50;
+
+        int qtdNotaVinte = reais / 20;
+        reais = reais % 20;
+
+        int qtdNotaDez = reais / 10;
+        reais = reais % 10;
+
+        int qtdNotaCinco = reais / 5;
+        reais = reais % 5;
+
+        int qtdNotaDois = reais / 2;
+        reais = reais % 2;
+
+        int qtdMoedaUmReal = reais;
+
+        // Centavos
+
+        int qtdCinquentaCentavos = centavos / 50;
+        centavos = centavos % 50;
+
+        int qtdVinteCincoCentavos = centavos / 25;
+        centavos = centavos % 25;
+
+        int qtdDezCentavos = centavos / 10;
+        centavos = centavos % 10;
+
+        int qtdCincoCentavos = centavos / 5;
+        centavos = centavos % 5;
+
+        int qtdUmCentavo = centavos;
+
+
+        System.out.println("Notas de R$100,00: " +qtdNotaCem);
+        System.out.println("Notas de R$50,00: " +qtdNotaCinquena);
+        System.out.println("Notas de R$20,00: " +qtdNotaVinte);
+        System.out.println("Notas de R$10,00: " +qtdNotaDez);
+        System.out.println("Notas de R$5,00: " +qtdNotaCinco);
+        System.out.println("Notas de R$2,00: " +qtdNotaDois);
+        System.out.println("Moedas de R$1,00: " +qtdMoedaUmReal);
+
+        System.out.println();
+
+        System.out.println("Moedas de R$0,50: " +qtdCinquentaCentavos);
+        System.out.println("Moedas de R$0,25: " +qtdVinteCincoCentavos);
+        System.out.println("Moedas de R$0,10: " +qtdDezCentavos);
+        System.out.println("Moedas de R$0,05: " +qtdCincoCentavos);
+        System.out.println("Moedas de R$0,01: " +qtdUmCentavo);
+
+
+
+
+
+
+
+
+
 
 
 
