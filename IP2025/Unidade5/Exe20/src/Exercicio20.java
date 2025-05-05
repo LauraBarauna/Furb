94b7255115c45d504debf5a67b2f0728f93fe760
Laura Barauna <lauravbarauna@gmail.com>
2025-05-05 10:47:06 -0300

    feat: add exercicio20 from unidade5

---
+++ IP2025/Unidade5/Exe20/src/Exercicio20.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a massa inicial em KG: ");
+        double kg = sc.nextDouble();
+        
+        int segundos = 0;
+
+        System.out.println("Massa inicial: " + kg + " kg");
+
+
+        while(kg >= 0.5) {
+            kg = kg / 2;
+            segundos += 50;
+        }
+
+        System.out.println("Massa final: " + kg + " kg");
+        System.out.println("Tempo em segundos que chegou na massa final: " + segundos);
+
+
+
+    }
+}
