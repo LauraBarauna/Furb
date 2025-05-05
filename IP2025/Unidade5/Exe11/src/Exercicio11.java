3ab11e082c39ac1f89f697910c7220036cfdb7a5
Laura Barauna <lauravbarauna@gmail.com>
2025-05-05 07:43:01 -0300

    feat: add exercicio 11 from unidade 5

--- IP2025/Unidade5/Exe11/src/Exercicio11.java
+++ IP2025/Unidade5/Exe11/src/Exercicio11.java
@@ -1,5 +1,18 @@
 public class Exercicio11 {
     public static void main(String[] args) {
-        
+
+        int biscoito = 0;
+
+        for (int i = 0; i <= 16; i++) {
+
+            if (biscoito >= 3) {
+                biscoito = biscoito * 3;
+            } else {
+                biscoito += i;
+                biscoito += 1;
+            }
+
+            System.out.println("HORA: " + (i + 1) + ", BISCOITOS QUEBRADOS: " + biscoito);
+        }
     }
 }
